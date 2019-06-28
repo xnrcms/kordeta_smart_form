009820d5bb854c3642ba4f35177538e69f83dd2a
562909771 <562909771@qq.com>
2019-06-28 10:30:47 +0800

    20109-06-28

--- application/common/lang/zh-cn/devmenu.php
+++ application/common/lang/zh-cn/devmenu.php
@@ -8,4 +8,5 @@ return [
     // 系统错误提示
     'notice_operation_type'                   	=> '数据操作类型错误',
     'notice_have_children' 						=> '该菜单下有子菜单，请先删除子菜单'
+    'notice_owner_children' 					=> '不能添加自己作为上级菜单'
 ];

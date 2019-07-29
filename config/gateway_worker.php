bd48e5167692a5229c8139c7fb146bd0581600e9
562909771 <562909771@qq.com>
2019-07-29 15:13:13 +0800

    2019-07-29 KDSV1.3迭代

--- config/gateway_worker.php
+++ config/gateway_worker.php
@@ -15,7 +15,7 @@ return [
     // 扩展自身需要的配置
     'protocol'              => 'websocket', // 协议 支持 tcp udp unix http websocket text
     'host'                  => '0.0.0.0', // 监听地址
-    'port'                  => 2348, // 监听端口
+    'port'                  => 8080, // 监听端口
     'socket'                => '', // 完整监听地址
     'context'               => [], // socket 上下文选项
     'register_deploy'       => true, // 是否需要部署register
@@ -26,7 +26,7 @@ return [
     'registerAddress'       => '127.0.0.1:1236',
 
     // Gateway配置
-    'name'                  => 'thinkphp',
+    'name'                  => 'KDSGETW',
     'count'                 => 1,
     'lanIp'                 => '127.0.0.1',
     'startPort'             => 2000,
